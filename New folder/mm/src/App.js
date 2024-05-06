805d4b9e6de4bf8c25dabfe1c424beb308a15974
Gyanranjan Patra <hi.gyanaranjanpatra@gmail.com>
2024-05-06 14:45:28 +0530

    updated

--- New folder/mm/src/App.js
+++ New folder/mm/src/App.js
@@ -1,17 +1,9 @@
-import './App.css';
-import {Header,Header2} from './Components/Header'
-
-import Main from './Components/Header';
-
-
-
-
+import Header from './Components/Header';
 function App() {
   return (
     <div>
       <Header/>
-      <Header2/>
-      <Main/>
+
     </div>
   );
 }
--- New folder/mm/src/Components/Header.jsx
+++ New folder/mm/src/Components/Header.jsx
@@ -1,21 +1,15 @@
+import React from 'react'
 
-function Header() {
-    return <div><h1>
-        Header 1</h1></div>
+const Header = () => {
+    return (
+        <nav>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+            <h1>Home</h1>
+        </nav>
+    )
+}
 
-};
-function Header2() {
-    return <div><h1>  This Is Header 2
-    </h1></div>
-
-};
-function Main() {
-    return <div><h1> This Is Main Components
-    </h1></div>
-
-};
-
-
-export { Header, Header2 };
-export default Main;
-
+export default Header
